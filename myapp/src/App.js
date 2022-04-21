d885912e7c87c2bdb62155fa2cf337dddc092e1d
Rajesh Vilasagaram <rajeshvelas0546.rn@gmail.com>
2022-04-21 15:03:32 +0530

    initial-commit

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Bmi from "./BMI/Bmi"
+
+function App() {
+  return (
+    <div className="App">
+      <Bmi />
+    </div>
+  );
+}
+
+export default App;
